f4ced24530cb502c4b3a811fc7fd4a7b74dc3910
luisvaquin <luisrodrigo2330@gmail.com>
2024-11-05 11:58:07 -0600

    add cors and json

--- src/app.js
+++ src/app.js
@@ -1,26 +1,33 @@
-import express from 'express'
-import taskRoutes from './routes/tasks.routes.js'
-import indexRoutes from './routes/index.routes.js'
-import loginRoutes from './routes/loginUsesr.routes.js'
-import transferRoutes from './routes/transfers.routes.js'
-import accountRoutes from './routes/account.routes.js'
+import express from 'express';
+import cors from 'cors'; // Importa el paquete CORS
 
-const app = express()
+import taskRoutes from './routes/tasks.routes.js';
+import indexRoutes from './routes/index.routes.js';
+import loginRoutes from './routes/loginUsesr.routes.js';
+import transferRoutes from './routes/transfers.routes.js';
+import accountRoutes from './routes/account.routes.js';
 
-app.use(express.json()) //Fuction express y factorizar objetos tipo JSON
+const app = express();
 
+// Configura CORS para permitir solicitudes desde el frontend
+app.use(cors({
+    origin: 'http://localhost:5173', // Permite solicitudes desde localhost:5173
+}));
 
-//Inicializacion de routes
-app.use('/api', indexRoutes)
-app.use('/api', loginRoutes)
-app.use('/api', accountRoutes)
-app.use('/api', transferRoutes)
-app.use('/api', taskRoutes)
+app.use(express.json()); // Middleware para parsear JSON
 
+// Inicialización de rutas
+app.use('/api', indexRoutes);
+app.use('/api', loginRoutes);
+app.use('/api', accountRoutes);
+app.use('/api', transferRoutes);
+app.use('/api', taskRoutes);
+
+// Middleware para manejar rutas no encontradas
 app.use((req, res, next) => {
     res.status(404).json({
         message: 'ROUTE NOT FOUND'
-    })
-})
+    });
+});
 
-export default app
+export default app;
